f74d301f6267ffe2315b0f6f5d91a4aa53163328
jamaliela <jamaliela@github.com>
2020-06-16 12:28:55 -0400

    working on html refining

--- src/main/java/edu/ucar/cisl/sagesiparcsmetadatasearch/repository/MetadataSearchRepository.java
+++ src/main/java/edu/ucar/cisl/sagesiparcsmetadatasearch/repository/MetadataSearchRepository.java
@@ -1,6 +1,61 @@
 package edu.ucar.cisl.sagesiparcsmetadatasearch.repository;
 
+import edu.ucar.cisl.sagesiparcsmetadatasearch.controller.MetadataSearchConfig;
+import edu.ucar.cisl.sagesiparcsmetadatasearch.model.Metadata;
+import edu.ucar.cisl.sagesiparcsmetadatasearch.model.MetadataSearchResults;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.request.QueryRequest;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.naming.directory.SearchResult;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
 public class MetadataSearchRepository {
 
+    private MetadataSearchConfig metadataSearchConfig;
+    private String solrUrl;
 
+    @Autowired
+    public MetadataSearchRepository(MetadataSearchConfig metadataSearchConfig, @Value("${spring.data.solr.host}") String solrUrl) {
+        this.metadataSearchConfig = metadataSearchConfig;
+        this.solrUrl = solrUrl;
+    }
+
+    public MetadataSearchResults getQueryResults(String queryText) throws IOException, SolrServerException {
+
+        MetadataSearchResults metadataSearchResults = new MetadataSearchResults();
+        SolrQuery query = new SolrQuery();
+        query.setQuery(queryText);
+        QueryResponse response = this.metadataSearchConfig.getSolrClient(this.solrUrl).query(query);
+        SolrDocumentList results = response.getResults();
+        metadataSearchResults.setNumFound(results.getNumFound());
+        List<Metadata> solrItems = new ArrayList<>();
+        for (SolrDocument result : results) {
+
+            Metadata solrItem = new Metadata();
+
+            solrItem.setTitle((String) result.get("title"));
+            solrItem.setDescription((String) result.get("description"));
+            solrItem.setResourceType((String) result.get("resource_type"));
+            solrItem.setDoi((String) result.get("doi"));
+            solrItems.add(solrItem);
+        }
+
+        metadataSearchResults.setMetadataResultList(solrItems);
+
+
+        return metadataSearchResults;
+
+    }
 }
